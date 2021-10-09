66067a7cc939a33f9b7711990da0a59acfa6d8a4
Bhashana Sadanjana <bhashana1999@gmail.com>
2021-10-09 12:42:33 +0530

    create Item stage

---
+++ src/controller/ItemFormController.java
@@ -0,0 +1,19 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import model.Item;
+
+import java.sql.SQLException;
+
+public class ItemFormController {
+
+
+    public TextField txtItemName;
+    public TextField txtItemCode;
+    public TextField txtItemQty;
+    public TextField txtItemPrice;
+
+    public void ItemSaveOnAction(ActionEvent actionEvent) {
+    }
+}
