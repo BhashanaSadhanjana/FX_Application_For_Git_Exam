9c01dd8d94bfde592109a909ef861db6f85ad3cf
Bhashana Sadanjana <bhashana1999@gmail.com>
2021-10-09 12:34:40 +0530

    create Customer stage

---
+++ src/controller/CustomerFormController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+import model.Customer;
+
+import java.sql.SQLException;
+
+public class CustomerFormController {
+
+    public TextField txtCusId;
+    public TextField txtCusName;
+    public TextField txtCusAddress;
+
+    public void CustomerSaveOnAction(ActionEvent actionEvent) {
+    }
+
+}
